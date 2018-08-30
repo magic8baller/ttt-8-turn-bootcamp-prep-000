b5f467b3644ccedf18cca4a5dc34cf32656c4f46
Leslie Turis <lturis@gmail.com>
2018-08-30 22:05:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     display_board(board)
-    move(board, index, player)
+    move(board, index)
   else
     turn(board)
   end
